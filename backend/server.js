17af072581243cd9d18e2bb17183d0a78c86639e
tejas sonune <tejass.9991@gmail.com>
2025-08-30 17:42:28 +0530

    fix bugs

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/projects', require('./routes/projects'));
-app.use('/api/tasks', require('./routes/tasks'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/projects", require("./routes/projects"));
+app.use("/api/tasks", require("./routes/tasks"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3300;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
